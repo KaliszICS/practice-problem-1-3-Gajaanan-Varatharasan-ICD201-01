b486620abbd21ebf3af0284d80401d435661cc1c
Gajaanan-Varatharasan-ICD201-01 <gajaanan.varatharasan@student.tdsb.on.ca>
2024-11-04 11:17:37 -0500

    Update PP1_3.py

--- PP1_3.py
+++ PP1_3.py
@@ -1,14 +1,19 @@
 #Write Assignment code here
 #Remember your code must be indented once
-print(" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *")
-print("\n* * * * *\n*       *\n*       *\n*       *\n* * * * *")
-print("\n* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *")
- #Write Assignment code here
-print("Hello" , end = "") 
-print(" World\n")
-
+def q1():
+  print(" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n")
+  print("\n* * * * *\n*       *\n*       *\n*       *\n* * * * *\n")
+  print("\n* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *\n")
+def q2():
   #Write Assignment code here
-print('"Man"')
-print('"Board"')
-print('"Man Overboard"')
+  print("Hello" , end = "") 
+  print(" World\n")
+def q3():
+  #Write Assignment code here
+  print('"Man"')
+  print('"Board"')
+  print('"Man Overboard"')
 #Do not alter anything after this line
+#q1()
+#q2()
+#q3()
