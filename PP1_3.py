370ec67a6148b9e2a01240e4255af6dc753be5ca
Gajaanan-Varatharasan-ICD201-01 <gajaanan.varatharasan@student.tdsb.on.ca>
2024-11-04 13:12:42 -0500

    Update PP1_3.py

--- PP1_3.py
+++ PP1_3.py
@@ -1,18 +1,20 @@
 #Write Assignment code here
 #Remember your code must be indented once
 def q1():
-  print(" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n")
-  print("\n* * * * *\n*       *\n*       *\n*       *\n* * * * *\n")
-  print("\n* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *\n")
+  print ( " * * * * * \n * * * * \n * * * * * *\n * * * * \n * * * * * *\n * * * * *\n" )
+
+  print ("* * * * \n       \n       \n       \n * * * *\n")
+
+  print ("* * * * * \n *       *\n       \n   *   *\n     *\n     *")
 def q2():
-  #Write Assignment code here
-  print("Hello" , end = "") 
-  print(" World\n")
+  print ("Hello", end = "")  
+  print (" World")
+
 def q3():
-  #Write Assignment code here
-  print('"Man"')
-  print('"Board"')
-  print('"Man Overboard"')
+  print ('"Man"\n"Board"\n"Man Overboard"')
+  
+
+
 #Do not alter anything after this line
 #q1()
 #q2()
